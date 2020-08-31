603a1c55293ade9ab2cf7c806aa4946ef1ca5f48
unknown <revosambat123@gmail.com>
2020-08-31 20:53:06 +0545

    Ems setup and working on template.

---
+++ README.md
@@ -0,0 +1,10 @@
+# django-ems
+practice of django in employee management system
+
+# Python virtual env commands:
+- **Create venv:** ``python -m venv env_name``
+- **Activate it:** ``cd .\env_name\Scripts\activate``
+- **Install package:** ``pip install <pkg>``
+- **Remove package:** ``pip uninstall <pkg-version>``
+- **List packages** ``pip list or pip freeze``
+- **DUmp package into file:** ``pip freeze > req.txt``s
---
+++ employee management system/poll/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+class Question(models.Model):
+    title = models.TextField(null=True, blank=True)
+    status = models.CharField(default='inactive', max_length=10)
+    created_by = models.ForeignKey(User, null=True, blank=True, on_delete=models.CASCADE)
+
+    start_date = models.DateTimeField(null=True, blank=True)
+    end_date = models.DateTimeField(null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+class Choice(models.Model):
+    question = models.ForeignKey('poll.Question', on_delete=models.CASCADE)
+    text = models.TextField(null=True, blank=True)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.text 
+    
