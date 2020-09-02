1b6920cb475fa826a8ce810d5de5f868cdef80f5
unknown <revosambat123@gmail.com>
2020-09-02 19:38:29 +0545

    Model Properties for count of polls

--- employee management system/poll/models.py
+++ employee management system/poll/models.py
@@ -15,6 +15,10 @@ class Question(models.Model):
     def __str__(self):
         return self.title
 
+    @property
+    def choices(self):
+        return self.choice_set.all()
+
 class Choice(models.Model):
     question = models.ForeignKey('poll.Question', on_delete=models.CASCADE)
     text = models.TextField(null=True, blank=True)
@@ -23,5 +27,18 @@ class Choice(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return self.text 
+        return self.text
+
+    @property
+    def votes(self):
+        return self.answer_set.count
+
+class Answer(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE) 
+    choice = models.ForeignKey(Choice, on_delete=models.CASCADE)
     
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.user.first_name + '-' + self.choice.text
