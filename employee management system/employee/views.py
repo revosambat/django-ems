8795c4fa6587dc728d82e24730536fb80c4efe22
unknown <revosambat123@gmail.com>
2020-09-05 17:06:51 +0545

    login and logout with djagno authentication

--- employee management system/employee/views.py
+++ employee management system/employee/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponseRedirect
 from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
 from django.urls import reverse
 from employee.form import UserForm
 # Create your views here.
@@ -50,4 +51,29 @@ def employee_delete(request, id=None):
     else:
         context = {}
         context['user'] = user
-        return render(request, 'employee/delete.html', context)        
+        return render(request, 'employee/delete.html', context)  
+
+def user_login(request):
+    context = {}
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user:
+            login(request, user)
+            return HttpResponseRedirect(reverse('user_success'))
+        else:
+            context["error"] = "Provide valid credentials!"
+            return render(request, "auth/login.html", context)
+    else:
+        return render(request, "auth/login.html", context)
+
+def success(request): 
+    context = {}
+    context['user'] = request.user
+    return render(request, "auth/success.html", context)
+
+def  user_logout(request):
+    if request.method == "POST":
+        logout(request)
+        return HttpResponseRedirect(reverse('user_login'))
--- employee management system/ems/urls.py
+++ employee management system/ems/urls.py
@@ -15,9 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from employee.views import user_login, user_logout, success
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('polls/', include('poll.urls')),
     path('employee/', include('employee.urls')),
+
+    path('login/', user_login, name="user_login"),
+    path('success/', success, name="user_success"),
+    path('logout/', user_logout, name="user_logout"),
 ]
